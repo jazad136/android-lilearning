0891d4a80c434e88083efeea5b2ceb8b1a0f7d15
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-12-09 20:57:59 -0500

    Add saySomething call to main activity

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfirstapp.utilities.MyClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myClass = MyClass()
+
+        myClass.saySomething()
+        // Command Backspace to delete
+        // Command D to duplicate
+
+    }
+
+    fun myFunc1() {
+
+    }
+    fun myFunc2() {
+
     }
 }
---
+++ app/src/main/java/com/example/myfirstapp/utilities/MyClass.kt
@@ -0,0 +1,9 @@
+package com.example.myfirstapp.utilities
+
+import android.util.Log
+const val TAG = "my_tag"
+class MyClass {
+    fun saySomething() {
+        Log.d(TAG, "saySomething: Hello!")
+    }
+}
